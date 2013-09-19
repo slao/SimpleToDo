7176e3a1d1c04cb2e1990109f771dce5104d4563
Sean Lao <slao258@gmail.com>
2013-09-18 23:47:31 -0700

    initial commit

---
+++ gen/slao/apps/simpletodo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package slao.apps.simpletodo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/simpletodo/TodoActivity.java
@@ -0,0 +1,82 @@
+package com.example.simpletodo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import slao.apps.simpletodo.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class TodoActivity extends Activity {
+	private ArrayList<String> items;
+	private ArrayAdapter<String> itemsAdapter;
+	private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.todo, menu);
+        return true;
+    }
+
+    public void addTodoItem(View v) {
+    	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    	saveItems();
+    }
+
+    private void setupListViewListener() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+    		@Override
+    		public boolean onItemLongClick(AdapterView<?> aView, View item, int pos, long id) {
+    			items.remove(pos);
+    			itemsAdapter.notifyDataSetChanged();
+    			saveItems();
+    			return true;
+    		}
+		});
+    }
+    
+    private void readItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		items = new ArrayList<String>(FileUtils.readLines(todoFile));
+    	} catch (IOException e) {
+    		items = new ArrayList<String>();
+    	}
+    }
+
+    private void saveItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		FileUtils.writeLines(todoFile, items);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}    	
+    }
+}
